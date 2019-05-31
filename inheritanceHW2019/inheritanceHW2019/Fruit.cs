52128fbf954ca6173fadaa43482739f8c7e58472
ickeith <50379970+ickeith@users.noreply.github.com>
2019-05-31 15:46:22 -0500

    Add files via upload

---
+++ inheritanceHW2019/inheritanceHW2019/Fruit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritanceHW2019
+{
+    public class Fruits
+    {
+        public string _fname { set; get; }
+        public string _color { set; get; }
+
+        public Fruits (string Fname, string color)
+        {
+            _fname = Fname;
+            _color = color ;
+            Console.WriteLine("Fresh fruit!");
+        }
+    }
+
+    public class Banana : Fruits
+    {
+        public int _weight { set; get; }
+            public string _flavor { set; get; }
+        public Banana(string color, int Weight,string flavor, string Fname ) : base (Fname,color)
+        {
+            _fname = Fname;
+            _color = color;
+            _weight = Weight;
+            _flavor = flavor;
+        }
+
+        public void Eaten(string fruitEaten)
+        {
+            Console.WriteLine("you ate {0}", _fname, fruitEaten);
+        }
+
+        public void peel(string PeeledFruit)
+        {
+            Console.WriteLine("you ate {0}", _fname, PeeledFruit);
+        }
+    }
+}
---
+++ inheritanceHW2019/inheritanceHW2019/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace inheritanceHW2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Banana BN1 = new Banana("yellow", 5, "Nasty", "potassium");
+            BN1.peel(Banana.)
+        }
+
+    }
+}
